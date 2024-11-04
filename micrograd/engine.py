8ef5df188c25f49daa0ce19e17827d429b6318d9
abhagavathi <amol-bhagavathi@uiowa.edu>
2024-11-04 16:47:07 -0600

    adding the operator methods

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -4,9 +4,20 @@ class Value:
     def __init__(self, data):
         self.data = data
 
+    # this is so that it prints the value of the data instead of the object address
     def __repr__(self) -> str:
         return f"Value(data={self.data}"
     
+    # overloading methods, python interprets the + operator as a call to the __add__ method
+    def __add__(self, other):
+        return Value(self.data + other.data)
+    
+    def __mul__(self, other):
+        return Value(self.data * other.data)
+    
+
 a = Value(3.0)
 b = Value(4.0)
-c = a + b
+d = Value(5.0)
+c = a + b * d
+print(c)
