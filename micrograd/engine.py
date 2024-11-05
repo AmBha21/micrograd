6c671b109dd9aee13960e12e7e80d27055319194
abhagavathi <amol-bhagavathi@uiowa.edu>
2024-11-05 14:55:56 -0600

    adding the operator methods

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,23 +1,25 @@
 import numpy as np
 
 class Value:
-    def __init__(self, data):
+    def __init__(self, data, children=(), op=''):
         self.data = data
+        self.prev = set(children)
+        self.op = op
 
-    # this is so that it prints the value of the data instead of the object address
+    # prints the value of the data instead of the object address
     def __repr__(self) -> str:
         return f"Value(data={self.data}"
     
     # overloading methods, python interprets the + operator as a call to the __add__ method
     def __add__(self, other):
-        return Value(self.data + other.data)
+        return Value(self.data + other.data, (self, other), '+')
     
     def __mul__(self, other):
-        return Value(self.data * other.data)
+        return Value(self.data * other.data, (self, other), '*')
     
 
 a = Value(3.0)
 b = Value(4.0)
 d = Value(5.0)
 c = a + b * d
-print(c)
+print(c.prev)
